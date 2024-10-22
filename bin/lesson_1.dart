7ea4da7ff47d7c45cf42a7a7e2444be795cc188b
danchik2009d <danielsabatbekova@gmail.com>
2024-10-22 17:57:48 +0300

    first

---
+++ bin/lesson_1.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments) {
+  double b = 4.20;
+  int a = 3;
+  print(b + a);
+
+  String text = 'name';
+  text = 'flutter';
+  print('I want to learn $text');
+
+  double myNumber = 2.225;
+
+  String myText;
+  myText = myNumber.toString();
+  print(myText);
+}
